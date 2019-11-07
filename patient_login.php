b166f0f2c60d33864fbb1ed4f790d98c38381865
mushrifshahreyar <mushrifshahreyar@gmail.com>
2019-11-08 00:22:46 +0530

    patient login

--- patient_login.php
+++ patient_login.php
@@ -1,38 +1,49 @@
 <!DOCTYPE HTML>
 <?php
-  session_start()
+  session_start();
+  $_SESSION['patient_id'] = null;
 ?>
 
 <?php
+  if(isset($_POST['patient_login'])) {
+    $servername = 'localhost';
+    $dbname = 'hospitalManagement';
+    $username = 'root';
+    $password = 'root';
 
-  $servername = 'localhost';
-  $dbname = 'hospitalManagement';
-  $username = 'root';
-  $password = 'root';
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-  if(!$conn) {
-    die("Connection failed : ". mysqli_connect_error());
-  }
-
-  if(isset($_POST['username'])) {
-    $uname = $_POST['username'];
-    $upass = $_POST['password'];
-
-
-    $sql = "SELECT * FROM patient WHERE patientID = $uname AND Pwd = $upass LIMIT 1";
-    $result = mysqli_query($sql);
-
-    if(mysqli_num_rows($result) == 1){
-
+    if(!$conn) {
+      die("Connection failed : ". mysqli_connect_error());
     }
     else {
+      $uname = $_POST['username'];
+      $upass = $_POST['password'];
+      $upass = md5($upass);
 
+      $sql = "SELECT * FROM patient WHERE email = '$uname'";
+      $result = mysqli_query($conn, $sql);
+
+      if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_assoc($result)) {
+          if($row['Pwd'] == $upass) {
+            $_SESSION['patient_id'] = $row['patientID'];
+            header("Location:patient_page.php");
+          }
+          else {
+            echo "<p class = login_verification>Invalid password</p>";
+          }
+        }
+      }
+      else {
+        echo "<p class = login_verification>Invalid email</p>";
+      }
     }
+  
+    $conn->close();
   }
 
-  $conn->close();
+  
 
 ?>
 
@@ -45,11 +56,11 @@
   <body>
 
     <h1>Welcome to the Health Centre</h1>
-    <form class="box" action="patient_page.php" method="POST">
+    <form class="box" action="#" method="POST">
       <h1>Patient Login</h1>
       <input type="text" name = "username" placeholder="Username">
       <input type="password" name = "password" placeholder="Password">
-      <input type="submit" name = "" value="Login">
+      <input type="submit" name = "patient_login" value="Login">
     </form>
   </body>
 </html>
---
+++ reg_admin copy.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Registration Admin</title>
+    <link rel = "stylesheet" type = "text/css" href = "reg_admin.css">
+    
+</head>
+
+<body>
+    <h1>Welcome to Registration Administration</h1>
+    <a href = "register_patient.php">
+    <button type="button">Patient Registration</button>
+    </a>
+
+    <a href = "register_doctor.php">
+    <button type="button">Doctor Registration</button> 
+    </a>
+    
+    <a href = "medicine_page.php">
+    <button type="button">Insert Medicine</button> 
+    </a>
+
+    <a href = "medicine_details.php">
+    <button type="button">Medicine Details</button> 
+    </a>
+
+    <a href = "test_page.php">
+    <button type="button">Insert Test Name</button> 
+    </a>
+</body>
+
+
+</html>
