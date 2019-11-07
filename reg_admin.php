3e09add3e854a2ad7c2f15d4132192e3983bbef0
Sonaal P. Pradeep <kingoftheland28@gmail.com>
2019-11-07 23:57:22 +0530

    Removed copies with Origs

--- reg_admin.php
+++ reg_admin.php
@@ -3,6 +3,8 @@
 <html>
 <head>
     <title>Registration Admin</title>
+    <link rel = "stylesheet" type = "text/css" href = "reg_admin.css">
+    
 </head>
 
 <body>
@@ -10,10 +12,21 @@
     <a href = "register_patient.php">
     <button type="button">Patient Registration</button>
     </a>
-    <br>
-    <br>
+
     <a href = "register_doctor.php">
     <button type="button">Doctor Registration</button> 
+    </a>
+    
+    <a href = "medicine_page.php">
+    <button type="button">Insert Medicine</button> 
+    </a>
+
+    <a href = "medicine_details.php">
+    <button type="button">Medicine Details</button> 
+    </a>
+
+    <a href = "test_page.php">
+    <button type="button">Insert Test Name</button> 
     </a>
 </body>
 
