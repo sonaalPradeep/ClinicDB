56b93e48dacb79f5b3ca76c7946925ec99902691
Sonaal P. Pradeep <kingoftheland28@gmail.com>
2019-11-09 14:59:57 +0530

    Minor Update

--- reg_admin.php
+++ reg_admin.php
@@ -1,33 +1,34 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 
 <html>
 <head>
-    <title>Registration Admin</title>
-    <link rel = "stylesheet" type = "text/css" href = "reg_admin.css">
-    
+    <title>Main Admin Page</title>
+    <link rel="stylesheet" type ="text/css" href="reg_admin.css">
 </head>
 
 <body>
-    <h1>Welcome to Registration Administration</h1>
+  <div class = "main" style="text-align:center">
+    <h1>Clinic Admin Page</h1>
     <a href = "register_patient.php">
     <button type="button">Patient Registration</button>
     </a>
 
     <a href = "register_doctor.php">
-    <button type="button">Doctor Registration</button> 
+    <button type="button">Doctor Registration</button>
     </a>
-    
+
     <a href = "medicine_page.php">
-    <button type="button">Insert Medicine</button> 
+    <button type="button">Insert Medicine</button>
     </a>
 
     <a href = "medicine_details.php">
-    <button type="button">Medicine Details</button> 
+    <button type="button">Medicine Details</button>
     </a>
 
     <a href = "test_page.php">
-    <button type="button">Insert Test Name</button> 
+    <button type="button">Insert Test Name</button>
     </a>
+  </div>
 </body>
 
 
